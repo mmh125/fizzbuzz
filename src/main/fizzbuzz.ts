8e4762d106f5ea90ae50c0f627e3979a9e4e1964
muna.hussein <muna.hussein@cazoo.co.uk>
2020-12-11 15:45:28 +0000

    mob next [ci-skip]

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,5 +1,4 @@
 export const fizzbuzz = (number: number): string => {
-  if (number == 3) return "fizz";
-  else if (number == 6) return "fizz";
+  if (number%3 === 0) return "fizz"
   else return "buzz";
 };
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -16,4 +16,8 @@ describe("fizzbuzz", () => {
   it("should return fizz when given 9", () => {
     expect(fizzbuzz(9)).toBe("fizz");
   });
+
+  it("should return 1 when given 1", () => {
+    expect(fizzbuzz(1)).toBe("1");
+  });
 });
