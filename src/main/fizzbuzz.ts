68f93e4f7d223a7082c0a3b6a70b424caea3df77
adrienbaron <adrien@abaron.net>
2020-12-11 15:41:56 +0000

    mob next [ci-skip]

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,6 +1,5 @@
 export const fizzbuzz = (number: number): string => {
-  if (number == 3)
-    return "fizz";
-  else
-    return "buzz";
+  if (number == 3) return "fizz";
+  else if (number == 6) return "fizz";
+  else return "buzz";
 };
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -4,11 +4,16 @@ describe("fizzbuzz", () => {
   it("should return fizz when given 3", () => {
     expect(fizzbuzz(3)).toBe("fizz");
   });
+
   it("should return buzz when given 5", () => {
     expect(fizzbuzz(5)).toBe("buzz");
   });
 
   it("should return fizz when given 6", () => {
     expect(fizzbuzz(6)).toBe("fizz");
+  });
+
+  it("should return fizz when given 9", () => {
+    expect(fizzbuzz(9)).toBe("fizz");
   });
 });
